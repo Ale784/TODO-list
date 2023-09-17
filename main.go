4dcd6b000663894d0771336730eb3fbda26564fb
Ale784 <acorrea7412@gmail.com>
2023-09-17 01:30:38 -0500

    feat: Implemented display of todo details and todo list

--- main.go
+++ main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"fmt"
 	"html/template"
 	"io"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-// Struct TODO
 type Todo struct {
-	Title        string
-	Completed    bool
-	OnHold       bool
-	PlanToFinish bool
-	Doing        bool
+	Id        string
+	Title     string
+	Detail    string
+	Completed bool
 }
 
 type Content struct {
@@ -34,23 +34,82 @@ func (t TemplateRenderer) Render(w io.Writer, name string, data interface{}, c e
 	return t.tmpl.ExecuteTemplate(w, name, data)
 }
 
+type ProjectPath struct {
+	ID string `param:"ID"`
+}
+
 func main() {
 
-	temp := template.Must(template.ParseFiles("index.html"))
+	temp := template.New("Index.html")
+
+	if _, err := temp.ParseFiles("./src/views/Index.html", "./src/views/Detail.html"); err != nil {
+		fmt.Println("OPPPS")
+	}
 
 	e := echo.New()
 
 	e.Renderer = NewTemplateRenderer(temp)
 	e.Use(middleware.Logger())
+	e.Use(middleware.RequestID())
+
+	ItemsTodo := []Todo{
+		{
+			Id:        "myfuckingid",
+			Title:     "Task 1",
+			Detail:    "Buy a new phone",
+			Completed: false,
+		},
+		{
+			Id:        "myFUckingTODONUmbert2o",
+			Title:     "Task 2",
+			Detail:    "Buy a new pc",
+			Completed: false,
+		},
+	}
 
 	e.GET("/", func(c echo.Context) error {
 		data := Content{
-			PageTitle: "My first go template",
-			Items: []Todo{
-				{Title: "Task 1", Completed: false, OnHold: false, PlanToFinish: false, Doing: false},
-			},
+			PageTitle: "List of todos",
+			Items:     ItemsTodo,
 		}
-		return c.Render(http.StatusOK, "index.html", data)
+		return c.Render(http.StatusOK, "Index.html", data)
+
+	})
+
+	e.GET("/Detail/:Id", func(c echo.Context) error {
+
+		path := new(ProjectPath)
+
+		if err := (&echo.DefaultBinder{}).BindPathParams(c, path); err != nil {
+			return err
+		}
+
+		projectID := path.ID
+		Item := []Todo{}
+
+		for Key, _ := range ItemsTodo {
+
+			i := ItemsTodo[Key]
+
+			if i.Id == projectID {
+
+				Item = []Todo{{
+					Id:        i.Id,
+					Title:     i.Title,
+					Detail:    i.Detail,
+					Completed: false,
+				}}
+
+			}
+
+		}
+
+		SingleData := Content{
+			PageTitle: "Details Todo",
+			Items:     Item,
+		}
+
+		return c.Render(http.StatusOK, "Detail.html", SingleData)
 
 	})
 
